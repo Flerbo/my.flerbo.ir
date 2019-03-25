788da441a76c49349f1235f40e2626f3ca5fb901
Javid Izadfar <J.Izadfar@Alibaba.ir>
2019-03-25 16:35:15 +0430

    added buy button

---
+++ components/build/controllers/order.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="reset row align-content-middle half-gutter gap"
+         @click="reset"
+    >
+        <svg-icon name="next" />
+        <span>ثبت سفارش</span>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        reset() {
+            this.$emit('click');
+        },
+    },
+};
+</script>
+<style lang="postcss" scoped>
+    @import '../../../assets/styles/vars/all.css';
+    .reset {
+        @util position(absolute, 0 0 null null);
+        background-color: $white;
+        border-radius: $round-radius;
+        color: $green;
+        cursor: pointer;
+        user-select: none;
+        transition: color $smooth-transition;
+        svg {
+            @util size(20px);
+            fill: $green;
+            transition: fill $smooth-transition;
+            margin-left: 10px;
+        }
+        &:hover {
+            color: $sazito;
+            svg {
+                fill: $sazito;
+            }
+        }
+    }
+</style>
