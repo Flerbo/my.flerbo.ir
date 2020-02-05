0d545a7eeeab7e0bf20ad1aca656dff63c0e2aaf
Javid-Izadfar <javid.izadfar@gmail.com>
2020-02-05 08:58:22 +0330

    feat: added empty preview page

---
+++ _nuxt/979b1c15938a6b8aed40.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{220:function(t,n,o){"use strict";o.r(n);var e={extends:o(184).a,layout:"empty",data:function(){return{isEmptyLayout:!0}}},l=o(4),component=Object(l.a)(e,void 0,void 0,!1,null,null,null);n.default=component.exports}}]);
