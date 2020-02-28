fa41caf9f4356a7b25b92fce63a609cfa12c668a
Javid-Izadfar <javid.izadfar@gmail.com>
2020-02-28 18:41:22 +0330

    update

--- _nuxt/8085b8ec65a6e33adc64.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{221:function(n,e,o){"use strict";o.r(e);var l={extends:o(184).a},t=o(4),component=Object(t.a)(l,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
